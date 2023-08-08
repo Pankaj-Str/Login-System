751c3af45fe961c7c971360aa17b47c5f951fbe7
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-08-08 14:13:08 +0530

    Add files via upload

---
+++ DB/Database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE loginsystem;
+
+use loginsystem;
+
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+select * from users;
